db427949f032b9698eccd088da1738034ae1ad74
Thays Oliveira <thaysoliveira14@hotmail.com>
2024-04-12 21:04:54 -0300

    criando as seções de dispositivos e faq

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
-/*document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function(){
     const buttons = document.querySelectorAll('[data-tab-button]')
+    const questions = document.querySelectorAll('[data-faq-question]')
 
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(btn) {
@@ -12,8 +13,19 @@
         })
     }
 
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreOuFechaResposta);
+    }
+
 }
 )
+
+function abreOuFechaResposta(elemento) {
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
+}
 
 function removeBotaoAtivo() {
     const buttons = document.querySelectorAll('[data-tab-button]')
@@ -29,54 +41,4 @@ function escondeTodasAbas() {
     for (let i = 0; i < tabsContainer.length; i++) {
         tabsContainer[i].classList.remove( 'shows__list--is-active' )
     }
-}*/
-
-//Versão simplificada
-// Aguarda o carregamento completo do DOM antes de executar o código
-document.addEventListener('DOMContentLoaded', () => {
-
-    // Seleciona todos os botões com o atributo 'data-tab-button'
-    const buttons = document.querySelectorAll('[data-tab-button]');
-
-    // Percorre cada botão e adiciona um evento de clique
-    buttons.forEach(button => {
-        button.addEventListener('click', (event) => {
-            
-        // Obtém o nome da aba alvo a partir do atributo 'data-tab-button' do botão clicado
-        const abaAlvo = event.target.dataset.tabButton;
-            
-        // Localiza a aba correspondente pelo atributo 'data-tab-id'
-        const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`);
-            
-        // Chama a função 'showAba' para exibir a aba correta
-        showAba(abaAlvo);
-        });
-    });
-    
-    // Função para exibir a aba especificada
-    function showAba(abaAlvo) {
-        
-      // Verifica se existe alguma aba ativa no momento
-        const abaAtiva = document.querySelector('.shows__list--is-active');
-        if (abaAtiva) {
-        // Remove a classe 'shows__list--is-active' da aba ativa atual
-        abaAtiva.classList.remove('shows__list--is-active');
-    }
-    
-      // Seleciona novamente a aba alvo
-    const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`);
-    
-      // Adiciona a classe 'shows__list--is-active' à aba alvo, tornando-a visível
-    aba.classList.add('shows__list--is-active');
-    
-      // Verifica se existe algum botão com a classe 'shows__tabs__button--is-active'
-    const botaoAtivo = document.querySelector('.shows__tabs__button--is-active');
-    if (botaoAtivo) {
-        // Remove a classe 'shows__tabs__button--is-active' do botão ativo anterior
-        botaoAtivo.classList.remove('shows__tabs__button--is-active');
-    }
-    
-      // Adiciona a classe 'shows__tabs__button--is-active' ao botão clicado, marcando-o como ativo
-    event.target.classList.add('shows__tabs__button--is-active');
-    }
-});
+}
