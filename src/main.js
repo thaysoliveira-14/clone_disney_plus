3ca44360ddac1f1d3249270dd6ef701897eb4827
Thays Oliveira <thaysoliveira14@hotmail.com>
2024-04-15 22:37:15 -0300

    ajuste no mobile

--- src/main.js
+++ src/main.js
@@ -2,6 +2,19 @@ document.addEventListener('DOMContentLoaded', function(){
     const buttons = document.querySelectorAll('[data-tab-button]')
     const questions = document.querySelectorAll('[data-faq-question]')
 
+    const heroSection = document.querySelector('.hero')
+    const alturaHero = heroSection.clientHeight
+
+    window.addEventListener('scroll', function() {
+        const posicaoAtual = window.scrollY;
+
+        if (posicaoAtual < alturaHero) {
+            ocultaElementosDoHeader();
+        } else {
+            exibeElementosDoHeader();
+        }
+    })
+
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(btn) {
             const abaAlvo = btn.target.dataset.tabButton
@@ -19,6 +32,16 @@ document.addEventListener('DOMContentLoaded', function(){
 
 }
 )
+
+function ocultaElementosDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function exibeElementosDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
 
 function abreOuFechaResposta(elemento) {
     const classe = 'faq__questions__item--is-open';
